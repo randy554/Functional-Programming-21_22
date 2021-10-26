82409d70254f5e885a0263cdc02d5137c7f40b34
randy554 <randy554@hotmail.com>
2021-10-26 12:08:19 +0200

    Added first letter uppercase function

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ let allPets = techTrackData.map((item) => {
 
 // Show all pets
 console.log("Show all pets:", allPets);
+
+// Change all pets with firstletter uppercase
+let upperCaseFirstLetter = allPets.map((item) => {
+  // console.log("Caps pet:", item.toUpperCase());
+  // console.log("Caps pet:", item.charAt(0).toUpperCase() + item.slice(1));
+  // console.log("Caps pet:", item.slice(1));
+  return item.charAt(0).toUpperCase() + item.slice(1);
+});
+
+// Show all pets with firstletter uppercase
+console.log("Pets with firstletter uppercase:", upperCaseFirstLetter);
