d439391eb40e7015e38d5f2d99647bce4ada00f1
randy554 <randy554@hotmail.com>
2021-10-28 21:56:34 +0200

    replaced local dataset file with fetch function
    - Added fetch function to get remote dataset
    - Placed function invocations in fetch.then()

--- index.js
+++ index.js
@@ -5,11 +5,100 @@ fetch(
   "https://raw.githubusercontent.com/cmda-tt/course-21-22/main/tech-track-dataset.json"
 )
   .then((result) => result.json())
-  .then((techTrackData) => console.log("Dataset from url:", techTrackData))
+  .then((techTrackData) => {
+    console.log("Dataset from url:", techTrackData);
+    return techTrackData;
+  })
+  .then((techTrackData) => {
+    // Show all favorite pet answers
+    console.log(
+      "Show all favorite pet answers:",
+      showAllAnswersFromQuestion(
+        techTrackData,
+        "Wat is je favoriete soort huisdier?"
+      )
+    );
+    return techTrackData;
+  })
+  .then((techTrackData) => {
+    let trimmedList = removeWhitespacFromList(
+      showAllAnswersFromQuestion(
+        techTrackData,
+        "Wat is je favoriete soort huisdier?"
+      )
+    );
+    console.log("Trim all answers", trimmedList);
+    return techTrackData;
+  })
+  .then((techTrackData) => {
+    // Show all pets with firstletter uppercase
+    console.log(
+      "Pet answers with firstletter uppercase:",
+      uppercaseFirstLetterAnswersFromList(
+        showAllAnswersFromQuestion(
+          techTrackData,
+          "Wat is je favoriete soort huisdier?"
+        )
+      )
+    );
+    return techTrackData;
+  })
+  .then((techTrackData) => {
+    // Show the first answers only from the list of answers
+    console.log(
+      "Pet answers with only one answer",
+      getFirstAnswerBasedOnContentsAnswer(
+        uppercaseFirstLetterAnswersFromList(
+          showAllAnswersFromQuestion(
+            techTrackData,
+            "Wat is je favoriete soort huisdier?"
+          )
+        ),
+        " of"
+      )
+    );
+    return techTrackData;
+  })
+  .then((techTrackData) => {
+    // Show list based on value
+    console.log(
+      "Show list based on value:",
+      filterOnAnimalValue(
+        getFirstAnswerBasedOnContentsAnswer(
+          uppercaseFirstLetterAnswersFromList(
+            showAllAnswersFromQuestion(
+              techTrackData,
+              "Wat is je favoriete soort huisdier?"
+            )
+          ),
+          " of"
+        ),
+        "Hond"
+      )
+    );
+    return techTrackData;
+  })
+  .then((techTrackData) => {
+    // Show list with a replaced value
+    console.log(
+      "A list with a replaced value:",
+      replaceValueFromList(
+        getFirstAnswerBasedOnContentsAnswer(
+          uppercaseFirstLetterAnswersFromList(
+            showAllAnswersFromQuestion(
+              techTrackData,
+              "Wat is je favoriete soort huisdier?"
+            )
+          ),
+          " of"
+        ),
+        "Dachshund",
+        "Hond"
+      )
+    );
+    return techTrackData;
+  })
   .catch((err) => console.log("Fetch failed!:", err));
-
-// Log data
-console.log("Tech track data:", techTrackData);
 
 // Get all answers from list
 let showAllAnswersFromQuestion = (list, question) => {
@@ -19,15 +108,6 @@ let showAllAnswersFromQuestion = (list, question) => {
   });
 };
 
-// Show all favorite pet answers
-console.log(
-  "Show all favorite pet answers:",
-  showAllAnswersFromQuestion(
-    techTrackData,
-    "Wat is je favoriete soort huisdier?"
-  )
-);
-
 // Remove witespace from listAnswers
 let removeWhitespacFromList = (list) => {
   return list.map((item) => {
@@ -35,31 +115,12 @@ let removeWhitespacFromList = (list) => {
   });
 };
 
-let trimmedList = removeWhitespacFromList(
-  showAllAnswersFromQuestion(
-    techTrackData,
-    "Wat is je favoriete soort huisdier?"
-  )
-);
-console.log("Trim all answers", trimmedList);
-
 // Change all answers from list to firstletter uppercase
 let uppercaseFirstLetterAnswersFromList = (list) => {
   return list.map((item) => {
     return item.charAt(0).toUpperCase() + item.slice(1);
   });
 };
-
-// Show all pets with firstletter uppercase
-console.log(
-  "Pet answers with firstletter uppercase:",
-  uppercaseFirstLetterAnswersFromList(
-    showAllAnswersFromQuestion(
-      techTrackData,
-      "Wat is je favoriete soort huisdier?"
-    )
-  )
-);
 
 // Get the first word of an answer if it contains a specific word/sign in answer
 let getFirstAnswerBasedOnContentsAnswer = (listAnswers, contents) => {
@@ -72,20 +133,6 @@ let getFirstAnswerBasedOnContentsAnswer = (listAnswers, contents) => {
   });
 };
 
-// Show the first answers only from the list of answers
-console.log(
-  "Pet answers with only one answer",
-  getFirstAnswerBasedOnContentsAnswer(
-    uppercaseFirstLetterAnswersFromList(
-      showAllAnswersFromQuestion(
-        techTrackData,
-        "Wat is je favoriete soort huisdier?"
-      )
-    ),
-    " of"
-  )
-);
-
 // Filter on animal value
 let filterOnAnimalValue = (list, animalValue) => {
   return list.filter((item) => {
@@ -94,23 +141,6 @@ let filterOnAnimalValue = (list, animalValue) => {
     }
   });
 };
-
-// Show list based on value
-console.log(
-  "Show list based on value:",
-  filterOnAnimalValue(
-    getFirstAnswerBasedOnContentsAnswer(
-      uppercaseFirstLetterAnswersFromList(
-        showAllAnswersFromQuestion(
-          techTrackData,
-          "Wat is je favoriete soort huisdier?"
-        )
-      ),
-      " of"
-    ),
-    "Hond"
-  )
-);
 
 // Replace a value from the list
 let replaceValueFromList = (list, replace, replaceWith) => {
@@ -124,21 +154,3 @@ let replaceValueFromList = (list, replace, replaceWith) => {
     }
   });
 };
-
-// Show list with a replaced value
-console.log(
-  "A list with a replaced value:",
-  replaceValueFromList(
-    getFirstAnswerBasedOnContentsAnswer(
-      uppercaseFirstLetterAnswersFromList(
-        showAllAnswersFromQuestion(
-          techTrackData,
-          "Wat is je favoriete soort huisdier?"
-        )
-      ),
-      " of"
-    ),
-    "Dachshund",
-    "Hond"
-  )
-);
