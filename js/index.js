a0103896b75649ce3f783046d7b7c9d5f2ffe519
randy554 <randy554@hotmail.com>
2021-11-07 20:36:54 +0100

    Refactored data cleaning functions in modules

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
+import {
+  getSourceFrmList,
+  listBySourceName,
+  removeWordFromValue,
+  uppercaseFirstLetterValueFromList,
+} from "./modules/dataCleaning.js";
+
 // Set API endpoint parameters
 let apiKey = "d08928de0d5d4809aef8375899851622";
 let phrases = "Ronaldo";
@@ -29,46 +36,3 @@ fetch(allNewsEndPoint)
     console.table("List by source:", newList);
   })
   .catch((err) => console.log(err));
-
-// Get source name from list
-let getSourceFrmList = (data) => {
-  return data.map((article) => {
-    console.log("Source:", article.source.name);
-    return article.source.name;
-  });
-};
-
-// Make lists by source name
-let listBySourceName = (list) => {
-  return list.reduce((sourceList, sourceName) => {
-    // If sourceName doesn't exist as an object property, then create new object property with a new array vallue & push sourceName into it as it's value
-    if (!sourceList.hasOwnProperty(sourceName)) {
-      sourceList[sourceName] = [sourceName];
-    } else {
-      // If it exist push another sourceName on to the array list
-      sourceList[sourceName].push(sourceName);
-    }
-
-    return sourceList;
-  }, {});
-};
-
-// Replace a value from the list
-let removeWordFromValue = (list, replace, replaceWith) => {
-  return list.map((item) => {
-    // If replace value is found in the list, replace it with the new value
-    if (item.includes(replace)) {
-      return item.replaceAll(replace, replaceWith);
-    } else {
-      // Else just return regular value
-      return item;
-    }
-  });
-};
-
-// Change all answers from list to firstletter uppercase
-let uppercaseFirstLetterValueFromList = (list) => {
-  return list.map((item) => {
-    return item.charAt(0).toUpperCase() + item.slice(1);
-  });
-};
