9a5131df45b8d0966dbd66a73a11a37280303292
randy554 <randy554@hotmail.com>
2021-11-07 18:50:52 +0100

    Update index.js
    - Added feat. to remove "www." from source name
    - Added feat. to uppercase first letter from source name
    - Added API endpoit to get news from 2 sources

--- js/index.js
+++ js/index.js
@@ -6,10 +6,13 @@ let sortBy = "relevancy";
 let pageSize = 100;
 let page = 1;
 let fullDataset = [];
-let endPoint = `https://newsapi.org/v2/everything?qInTitle=${phrases}&language=${language}&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`;
+let newsSource1 = "www.ad.nl";
+let newsSource2 = "telegraaf.nl";
+let allNewsEndPoint = `https://newsapi.org/v2/everything?qInTitle=${phrases}&language=${language}&page=${page}&pageSize=${pageSize}&apiKey=${apiKey}`;
+let fromTwoSourcesEndPoint = `https://newsapi.org/v2/everything?qInTitle=${phrases}&language=${language}&page=${page}&pageSize=${pageSize}&domains=${newsSource1},${newsSource2}&apiKey=${apiKey}`;
 
 // Get data from News API
-fetch(endPoint)
+fetch(allNewsEndPoint)
   .then((result) => result.json())
   .then((data) => {
     console.log("News articles:", data);
@@ -18,7 +21,11 @@ fetch(endPoint)
   .then((data) => {
     let sourceL = getSourceFrmList(data.articles);
     console.log("List with sources:", sourceL);
-    let newList = listBySourceName(sourceL);
+    let withoutW = removeWordFromValue(sourceL, "Www.", "");
+    console.log("List with wwww:", withoutW);
+    let capFirstL = uppercaseFirstLetterValueFromList(withoutW);
+    console.log("List with first letter cap:", capFirstL);
+    let newList = listBySourceName(capFirstL);
     console.table("List by source:", newList);
   })
   .catch((err) => console.log(err));
@@ -44,4 +51,24 @@ let listBySourceName = (list) => {
 
     return sourceList;
   }, {});
+};
+
+// Replace a value from the list
+let removeWordFromValue = (list, replace, replaceWith) => {
+  return list.map((item) => {
+    // If replace value is found in the list, replace it with the new value
+    if (item.includes(replace)) {
+      return item.replaceAll(replace, replaceWith);
+    } else {
+      // Else just return regular value
+      return item;
+    }
+  });
+};
+
+// Change all answers from list to firstletter uppercase
+let uppercaseFirstLetterValueFromList = (list) => {
+  return list.map((item) => {
+    return item.charAt(0).toUpperCase() + item.slice(1);
+  });
 };
